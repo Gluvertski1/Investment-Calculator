6682a8a274dbced49dd38bd4521a6455492684aa
Jared <73083139+Gluvertski1@users.noreply.github.com>
2021-12-12 16:35:15 -0500

    Add files via upload

--- InvCalc_Controller.py
+++ InvCalc_Controller.py
@@ -7,21 +7,54 @@ class Main(QMainWindow, InvCalc_Window):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+        self.calculate_Button.clicked.connect(self.calculate)
+        self.additional_Box.setPlainText("0")
 
+    def calculate(self):
+        # A = P(1+(r/n)^nt
+        # A - final amount
+        # P - Principle
+        # r - interest rate
+        # n - number of times interest applied per time period
+        # t - numebr of time periods elapsed
 
+        principle = int(self.initial_Box.toPlainText())
 
+        interest_rate = float(self.rate_Box.toPlainText())/100
+        t = int(self.years_Box.toPlainText())
+        n = self.comboBox.currentText()
 
+        additional = int(self.additional_Box.toPlainText())
 
+        if n == "Annually":
+            num_n = 1
+        elif n == "Semi-Annually":
+            num_n = 2
+        elif n == "Quarterly":
+            num_n = 4
+        elif n == "Montly":
+            num_n = 12
+        elif n == "Semi-Monthly":
+            num_n = 12*2
+        elif n == "Biweekly":
+            num_n = 52*2
+        elif n == "Weekly":
+            num_n = 52
+        else:
+            num_n = 365
 
+        N_T = num_n*t
+        A = principle*(1+(interest_rate/num_n))**N_T
 
+        if A > principle:
+            interest = A - principle - additional
+        else:
+            interest = principle - A - additional
 
-
-
-
-
-
-
-
+        self.balance_Label.setText(str("{:.2f}".format(A)))
+        self.start_Amount_Label.setText(self.initial_Box.toPlainText())
+        self.contributions_Label.setText(self.additional_Box.toPlainText())
+        self.Interest_Label.setText(str("{:.2f}".format(interest)))
 
 
 
